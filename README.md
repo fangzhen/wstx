c02a6b61c9ddb56f7f290b086dfff96dec9f815a
fangzhen <fangzhn@gmail.com>
2014-01-13 15:54:10 +0800

    removes useless client stub generated by wsimport; A mock Junit testcase for coordinator activation service, use Spring for ws client. Added Readme.

---
+++ README.md
@@ -0,0 +1,9 @@
+A WS-Coordination and WS-Transaction standards implementation based on Apache CXF.
+
+Building and running with maven
+mvn clean package
+
+For standalone tomcat, use
+mvn tomcat7:redeploy
+
+The tomcat port is set to 28080, Client is developed with Spring. see client-beans.xml.
--- src/test/java/info/fzhen/wstx/wscoor/TestActivationPort.java
+++ src/test/java/info/fzhen/wstx/wscoor/TestActivationPort.java
@@ -1,15 +1,22 @@
 package info.fzhen.wstx.wscoor;
 
 import static org.junit.Assert.*;
-import org.oasis_open.docs.ws_tx.wscoor._2006._06.ActivationService;
 
+import org.oasis_open.docs.ws_tx.wscoor._2006._06.ActivationPortType;
+import org.oasis_open.docs.ws_tx.wscoor._2006._06.CreateCoordinationContextResponseType;
+import org.oasis_open.docs.ws_tx.wscoor._2006._06.CreateCoordinationContextType;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.junit.Test;
 
 public class TestActivationPort {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				new String[]{"info/fzhen/wstx/wscoor/client-beans.xml"});
+		ActivationPortType port = (ActivationPortType)context.getBean("Activation");
+		CreateCoordinationContextResponseType res = port.createCoordinationContextOperation(new CreateCoordinationContextType());
+		assertEquals(res, null);
 	}
 
 }
