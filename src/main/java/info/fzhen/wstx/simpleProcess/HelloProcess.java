dee99a63bb52921e59e33753260b8a7944d33c19
fangzhen <fangzhn@gmail.com>
2014-05-12 22:46:23 +0800

    new build after refactoring of wstx-common

--- src/main/java/info/fzhen/wstx/simpleProcess/HelloProcess.java
+++ src/main/java/info/fzhen/wstx/simpleProcess/HelloProcess.java
@@ -1,9 +1,14 @@
 package info.fzhen.wstx.simpleProcess;
 
+import info.fzhen.wstx.Constants;
 import info.fzhen.wstx.HelloService;
 
 import javax.jws.WebService;
 
+import org.oasis_open.docs.ws_tx.wscoor._2006._06.ActivationPortType;
+import org.oasis_open.docs.ws_tx.wscoor._2006._06.CoordinationContext;
+import org.oasis_open.docs.ws_tx.wscoor._2006._06.CreateCoordinationContextResponseType;
+import org.oasis_open.docs.ws_tx.wscoor._2006._06.CreateCoordinationContextType;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @WebService
@@ -11,14 +16,20 @@ public class HelloProcess implements Process{
 	private HelloService helloSer;
 	ClassPathXmlApplicationContext context;
 	public HelloProcess() {
-		context = new ClassPathXmlApplicationContext(
-				new String[]{"client-beans.xml"});
-		helloSer = (HelloService)context.getBean("hello");
 	}
 
 	@Override
 	public void execute() {
-		System.out.println(helloSer.sayHello());
+		context = new ClassPathXmlApplicationContext(
+				new String[]{"client-beans.xml"});
+		helloSer = (HelloService)context.getBean("hello");
+		ActivationPortType activationSer = (ActivationPortType)context.getBean("activationService");
+		CreateCoordinationContextType ccc = new CreateCoordinationContextType();
+		ccc.setCoordinationType(Constants.WSAT);
+		CreateCoordinationContextResponseType res = activationSer.createCoordinationContextOperation(ccc);
+		CoordinationContext coordinationContext = res.getCoordinationContext();				
+		//then send application messages with CC
+		
 		context.close();
 	}
 	
