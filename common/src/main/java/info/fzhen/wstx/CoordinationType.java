b36cec0d72bd0e4d18fab118a099447053188723
fangzhen <fangzhenzyou@gmail.com>
2014-11-10 20:28:07 +0800

    add wsba protocol enumeration

--- common/src/main/java/info/fzhen/wstx/CoordinationType.java
+++ common/src/main/java/info/fzhen/wstx/CoordinationType.java
@@ -1,8 +1,12 @@
 package info.fzhen.wstx;
 
+/**
+ * Coordination Type enumerations that are supported
+ */
 public enum CoordinationType {
 	WSAT("http://docs.oasis-open.org/ws-tx/wsat/2006/06"),
-	WSBA("http://docs.oasis-open.org/ws-tx/wsba/2006/06");
+	WSBA_ATOMIC("http://docs.oasis-open.org/ws-tx/wsba/2006/06/AtomicOutcome"),
+	WSBA_MIXED("http://docs.oasis-open.org/ws-tx/wsba/2006/06/MixedOutcome");
 
 	private String text;
 
---
+++ common/src/main/java/info/fzhen/wstx/ba/BaProtocol.java
@@ -0,0 +1,38 @@
+package info.fzhen.wstx.ba;
+
+/**
+ * Business Activity Protocol enumerations
+ */
+public enum BaProtocol {
+	PARTICIPANT_COMPLETION
+			("http://docs.oasis-open.org/ws-tx/wsba/2006/06/ParticipantCompletion"),
+	COORDINATOR_COMPLETION(
+			"http://docs.oasis-open.org/ws-tx/wsba/2006/06/CoordinatorCompletion");
+
+	private String text;
+
+	private BaProtocol(String text) {
+		this.text = text;
+	}
+
+	public static BaProtocol fromString(String text) {
+		if (text != null) {
+			for (BaProtocol p : BaProtocol.values()) {
+				if (text.equals(p.text)) {
+					return p;
+				}
+			}
+		}
+		throw new IllegalArgumentException("No protocol of " + text + "defined in wsat coordiantion type");
+	}
+
+	@Override
+	public String toString() {
+		return text;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+}
