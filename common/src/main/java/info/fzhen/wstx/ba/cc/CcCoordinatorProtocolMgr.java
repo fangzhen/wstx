9be3ea930d4bd8be1a5548a4c364053032253557
fangzhen <fangzhenzyou@gmail.com>
2014-11-11 14:09:11 +0800

    ongoing

--- common/src/main/java/info/fzhen/wstx/ba/cc/CcCoordinatorProtocolMgr.java
+++ common/src/main/java/info/fzhen/wstx/ba/cc/CcCoordinatorProtocolMgr.java
@@ -1,7 +1,34 @@
 package info.fzhen.wstx.ba.cc;
 
+import info.fzhen.wstx.WstxRtException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * Coordinator completion coordinator side protocol manager
  */
 public class CcCoordinatorProtocolMgr {
+	private static final Log __LOG = LogFactory.getLog(CcCoordinatorProtocolMgr.class);
+
+	private static CcCoordinatorProtocolMgr instance;
+
+	public static CcCoordinatorProtocolMgr getInstance() {
+		if (instance == null){
+			String msg = "Business activity coordinator completion protocol " +
+					"service manager(" + CcCoordinatorProtocolMgr.class +
+					") singleton instance hasn't been initialized. It should be " +
+					"initialized by Spring as singleton. Please check your " +
+					"configuration file.";
+			if (__LOG.isErrorEnabled()){
+				__LOG.error(msg);
+			}
+			throw new WstxRtException(msg);
+		}
+		return instance;
+	}
+
+	public static void setInstance(CcCoordinatorProtocolMgr instance){
+		CcCoordinatorProtocolMgr.instance = instance;
+	}
+
 }
