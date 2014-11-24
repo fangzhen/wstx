dc24ab3cb3e9d5965650cd75217faef12511156b
fangzhen <fangzhenzyou@gmail.com>
2014-11-24 20:43:05 +0800

    bug fix of testcase

--- integration/src/main/java/info/fzhen/wstx/test/processes/AtProcessSubordinate.java
+++ integration/src/main/java/info/fzhen/wstx/test/processes/AtProcessSubordinate.java
@@ -2,7 +2,6 @@ package info.fzhen.wstx.test.processes;
 
 import info.fzhen.wstx.TransactionFactory;
 import info.fzhen.wstx.at.WsatTransaction;
-import info.fzhen.wstx.at.completion.AtInitiatorPartManager;
 import info.fzhen.wstx.test.ClientProxies;
 import info.fzhen.wstx.test.services.HelloService;
 import org.apache.commons.logging.Log;
@@ -28,10 +27,6 @@ public class AtProcessSubordinate extends TransactionalProcess implements Proces
 		ActivationPortType activationSer = clientProxies.getActivationService();
 		WsatTransaction transaction = TransactionFactory.getInstance().createWsatTransaction(activationSer);
 		transaction.begin();
-
-		//register WSAT completion protocol
-		AtInitiatorPartManager manager = AtInitiatorPartManager.getInstance();
-		manager.registerInitiator(transaction);
 
 		//then send application messages with CC
 		HelloService helloSer = clientProxies.getService("helloServiceSubordinate");
